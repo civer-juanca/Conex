0694d355ccf2f8bf990d0a20f1ee6ba5bd47254a
Juan Chacon <civerjuanca89@gmail.com>
2024-08-02 18:43:16 -0500

    Reconfiguracion del proyecto al instalar laravel/breeze

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\producto;
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
@@ -37,7 +37,7 @@ class ProductoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(producto $producto)
+    public function show(Producto $producto)
     {
         //
     }
@@ -45,7 +45,7 @@ class ProductoController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(producto $producto)
+    public function edit(Producto $producto)
     {
         return view('productos.edit', compact('producto'));
     }
@@ -62,7 +62,7 @@ class ProductoController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(producto $producto)
+    public function destroy(Producto $producto)
     {
         $producto->delete();
         return redirect()->route('productos.index');
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
-use App\Models\producto;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,15 +14,18 @@ use App\Models\producto;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::view('/','welcome')->name('home');
 
-Route::view('/', 'welcome'); 
+Route::resource('productos', ProductoController::class)->middleware('auth');
 
-// CRUD de los productos
-Route::resource('productos', ProductoController::class); //crea las 7 rutas para CRUD
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
+require __DIR__.'/auth.php';
